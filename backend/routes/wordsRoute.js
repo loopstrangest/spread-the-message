dc0ea7d41a34e178ae47257b3fb133c78a9c3d14
loopstrangest <loopstrangest@gmail.com>
2024-03-08 16:42:09 -0600

    Update english words import

--- backend/routes/wordsRoute.js
+++ backend/routes/wordsRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import englishWords from "an-array-of-english-words/index.json";
+import englishWords from "an-array-of-english-words";
 import { redisClient } from "../redis.js";
 
 const router = express.Router();
